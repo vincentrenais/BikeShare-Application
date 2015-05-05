12a5d120d78497afbb1a50c72d6941081a080de4
Vincent Renais <vincentrenais@gmail.com>
2015-05-05 18:37:39 -0400

    Commit

--- Model/StationManager.m
+++ Model/StationManager.m
@@ -36,7 +36,6 @@
          NSError *error = nil;
          
          // Deserialized the information we get from the API
-         
          NSDictionary *stationsListJSON = [NSJSONSerialization JSONObjectWithData:response options:0 error:&error];
          
          if (!error)
@@ -61,24 +60,18 @@
              }
              if (success)
              {
-//                 dispatch_sync(dispatch_get_main_queue(), ^{
-                     success([StationManager sharedList].arrayOfStations);
-//                 });
-                 
-                 return;
+                success([StationManager sharedList].arrayOfStations);
+                return;
              }
          }
          else
          {
              if (failure)
              {
-//                 dispatch_sync(dispatch_get_main_queue(), ^{
-                     success([StationManager sharedList].arrayOfStations);
-//                 });
+                success([StationManager sharedList].arrayOfStations);
              }
-             failure(error);
-             return;
-         
+                failure(error);
+                return;
          }
      }];
     
