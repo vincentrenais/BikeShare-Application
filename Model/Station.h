0e19cb24914ccda39ad2dc7adaa0e2a67218962a
Vincent Renais <vincentrenais@gmail.com>
2015-05-07 21:40:27 -0400

    Commit

--- Model/Station.h
+++ Model/Station.h
@@ -7,16 +7,17 @@
 //
 
 @import Foundation;
+@import MapKit;
 
-#import "Location.h"
-
-@interface Station : Location
+@interface Station : NSObject <MKAnnotation>
 
 @property (strong,nonatomic) NSString *stationName;
 @property (strong,nonatomic) NSNumber *availableDocks;
 @property (strong,nonatomic) NSNumber *availableBikes;
 @property (strong,nonatomic) NSString *statusValue;
-
-
+@property (strong,nonatomic) NSNumber *location;
+@property (strong,nonatomic) NSNumber *latitude;
+@property (strong,nonatomic) NSNumber *longitude;
+@property (nonatomic) CLLocationCoordinate2D coordinate;
 
 @end
